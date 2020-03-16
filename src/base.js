a1e8753e43cd9373b63188152ce8cafa763fd9fd
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-03-16 16:16:58 +0100

    set up firebase connection

--- src/base.js
+++ src/base.js
@@ -2,13 +2,12 @@ import * as firebase from 'firebase';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyAobLcWKdg8bmXVqQRPfB2o2IWxA51AWIQ",
-    authDomain: "african-masks-app.firebaseapp.com",
-    databaseURL: "https://african-masks-app.firebaseio.com",
-    projectId: "african-masks-app",
-    storageBucket: "african-masks-app.appspot.com",
-    messagingSenderId: "679233135635",
-    appId: "1:679233135635:web:01ce33a7298b9fcd248be6"
-})
-
+        apiKey: process.env.REACT_APP_apiKey,
+        authDomain: process.env.REACT_APP_authDomain,
+        databaseURL: process.env.REACT_APP_databaseURL,
+        projectId: process.env.REACT_APP_projectId,
+        storageBucket: process.env.REACT_APP_storageBucket,
+        messagingSenderId: process.env.REACT_APP_messagingSenderId,
+        appId:process.env.REACT_APP_appId
+    })
 export default app;
